41aa0a0f0ba73ffbc3268c24f5fe68c07690f239
Ricky Rojas <rickyrojas19@gmail.com>
2018-10-22 14:35:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 chocolateBars.push('smarties');
 console.log(chocolateBars);
 
-
+var cities = ['Boston', 'New York', 'Philly', 'Frisco'];
+cities.unshift('Oakland');
 
