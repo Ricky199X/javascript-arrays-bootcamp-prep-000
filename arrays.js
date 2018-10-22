3a8ea6734bb9dbabbe5f7380610dc4b3765edf1d
Ricky Rojas <rickyrojas19@gmail.com>
2018-10-22 16:16:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -54,6 +54,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array = array.length -1;
+  array = array.slice(0, array.length -1);
   return array;
 }
